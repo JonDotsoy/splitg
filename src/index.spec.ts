37ac3ae7ed3db2555122c78adba25ba0602f1890
Jonathan Delgado <hi@jon.soy>
2024-11-12 14:32:45 -0300

    tests: upgrade tests caces

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,39 +5,44 @@ it("splits a string by spaces", () =>
   expect(splitg(`abc def g`)).toEqual(["abc", "def", "g"]));
 it("splits a string by a custom separator", () =>
   expect(splitg(`abc~def~g`, "~")).toEqual(["abc", "def", "g"]));
-it("splits a string preserving curly braces", () =>
-  expect(splitg(`{abc def} g`)).toEqual(["{abc def}", "g"]));
-it("splits a string preserving parentheses", () =>
-  expect(splitg(`(abc def) g`)).toEqual(["(abc def)", "g"]));
-it("splits a string preserving square brackets", () =>
-  expect(splitg(`[abc def] g`)).toEqual(["[abc def]", "g"]));
-it("splits a string preserving nested square brackets", () =>
-  expect(splitg(`a [[abc [def]]] g`)).toEqual(["a", "[[abc [def]]]", "g"]));
 it("splits a string preserving backslashes", () =>
   expect(splitg(`abc\\ def g`)).toEqual(["abc\\ def", "g"]));
-it("splits a string preserving angle brackets (default)", () =>
-  expect(splitg(`<abc def> g`)).toEqual(["<abc", "def>", "g"]));
-it("splits a string preserving angle brackets (custom)", () =>
-  expect(splitg(`<abc def> g`, undefined, { brackets: [["<", ">"]] })).toEqual([
-    "<abc def>",
-    "g",
-  ])),
-  it("splits a string with trailing spaces", () =>
-    expect(splitg(`abc def g  `)).toEqual(["abc", "def", "g", "", ""]));
-it("splits a string with a json object", () =>
-  expect(
-    splitg(`this is a json { "name": "ok" } with a property name`),
-  ).toEqual([
-    `this`,
-    `is`,
-    `a`,
-    `json`,
-    `{ "name": "ok" }`,
-    `with`,
-    `a`,
-    `property`,
-    `name`,
-  ]));
+it("splits a string with trailing spaces", () =>
+  expect(splitg(`abc def g  `)).toEqual(["abc", "def", "g", "", ""]));
+
+describe("preserving brackets", () => {
+  it("splits a string preserving curly braces", () =>
+    expect(splitg(`{abc def} g`)).toEqual(["{abc def}", "g"]));
+  it("splits a string preserving parentheses", () =>
+    expect(splitg(`(abc def) g`)).toEqual(["(abc def)", "g"]));
+  it("splits a string preserving square brackets", () =>
+    expect(splitg(`[abc def] g`)).toEqual(["[abc def]", "g"]));
+  it("splits a string preserving nested square brackets", () =>
+    expect(splitg(`a [[abc [def]]] g`)).toEqual(["a", "[[abc [def]]]", "g"]));
+  it("splits a string preserving angle brackets (default)", () =>
+    expect(splitg(`<abc def> g`)).toEqual(["<abc", "def>", "g"]));
+  it("splits a string preserving angle brackets (custom)", () =>
+    expect(
+      splitg(`<abc def> g`, undefined, { brackets: [["<", ">"]] }),
+    ).toEqual(["<abc def>", "g"])),
+    it("splits a string with a json object", () =>
+      expect(
+        splitg(`this is a json { "name": "ok" } with a property name`),
+      ).toEqual([
+        `this`,
+        `is`,
+        `a`,
+        `json`,
+        `{ "name": "ok" }`,
+        `with`,
+        `a`,
+        `property`,
+        `name`,
+      ]));
+
+  it("splits a string preserving mixed brackets", () =>
+    expect(splitg(`a [ ( ] ) ] b`)).toEqual([`a`, `[ ( ]`, `)`, `]`, `b`]));
+});
 
 describe("parseSplitgOptions", () => {
   it("should parse a string splitter", () =>
